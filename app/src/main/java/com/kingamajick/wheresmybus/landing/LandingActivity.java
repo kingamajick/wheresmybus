aa82d8b633e5111a809e7c0d768edde6da980461
Rich King <kingamajick@gmail.com>
2015-11-07 16:36:02 +0000

    Initial project stucture

---
+++ app/src/main/java/com/kingamajick/wheresmybus/landing/LandingActivity.java
@@ -0,0 +1,15 @@
+package com.kingamajick.wheresmybus.landing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.kingamajick.wheresmybus.R;
+
+public class LandingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+    }
+}
